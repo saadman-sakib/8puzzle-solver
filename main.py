5d1193d1aabce29d5abb3cbf41824ecaff3f6027
Saadman Sakib <khubsadman@gmail.com>
2021-05-26 18:47:09 +0600

    ...

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from solver import Solver
 
-a = [
-    [6, 5,None],
-    [4,7,2],
-    [1,8,3]
-]
+a = [[3,None,2],
+	 [4,1,8],
+	 [6,5,7]]
+
+
 gem = Solver(a)
+gem.pretty_print(a)
 gem.solve()
--- solver.py
+++ solver.py
@@ -5,6 +5,7 @@ class Solver:
     def __init__(self, board):
         self.board = board
         self.result = None
+        self.explored = set()
         self.terminal_state = [
             [None,1,2],
             [3,4,5],
@@ -30,6 +31,8 @@ class Solver:
             pass
 
         try:
+            if i == 0:
+                raise Exeption
             new_board = copy.deepcopy(board)
             new_board[i][j] = board[i-1][j]
             new_board[i-1][j] = None
@@ -46,12 +49,15 @@ class Solver:
             pass
 
         try:
+            if j == 0:
+                raise Exeption
             new_board = copy.deepcopy(board)
             new_board[i][j] = board[i][j-1]
             new_board[i][j-1] = None
             moves.append((new_board,"LEFT"))
         except:
             pass
+
         return moves
 
     def heuristic(self, node):
@@ -62,49 +68,60 @@ class Solver:
                     h += 1
         return node.level + h
 
+    def pretty_print(self, board):
+        print("------")
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == None:
+                    print(" ", sep="|",end="|")
+                else:
+                    print(board[i][j], sep="|",end="|")
+            print()
+        print("------")
+
+    def ending(self, node):
+        actions = []
+        states  = []
+        while node.parent is not None:
+            states.append(node.state)
+            actions.append(node.action)
+            node = node.parent
+        states.reverse()
+        actions.reverse()
+        print("\n\n\n")
+        print("Solution:")
+        for state, action in zip(states,actions):
+            print(action," =>")
+            self.pretty_print(state)
+
+    def in_explored(self, state):
+        for node in self.explored:
+            if node.state == state:
+                return True
+        return False
+
     def solve(self):
         print("Solving...")
         q = Queue()
-        explored = set()
-        start = Node(parent=None, state=self.board, level=0, action=None)
+        start = Node(parent=None,
+        			 state=self.board,
+        			 level=0,
+        			 action=None)
         q.add(start)
-
-        def in_explored(state):
-            for node in explored:
-                if node.state == state:
-                    return True
-            return False
-
-        i = 0
 
         while True:
             q.queue.sort(key = lambda x:self.heuristic(x),reverse=False)
             node = q.remove()
-            explored.add(node)
+            self.explored.add(node)
 
             if node.state == self.terminal_state:
-                actions = []
-                states  = []
-                while node.parent is not None:
-                    states.append(node.state)
-                    actions.append(node.action)
-                    node = node.parent
-                states.reverse()
-                actions.reverse()
-                print("\n\n\n")
-                print("Solution:")
-                for state, action in zip(states,actions):
-                    print(action)
-                    for row in state:
-                        print(row)
-                    print("\\/")
+                self.ending(node)
                 return None
 
             for state, action in self.available_moves(node.state):
-                if not(q.contains_state(state)) and not(in_explored(state)):
+                if not(q.contains_state(state)) and not(self.in_explored(state)):
                     child = Node(parent=node, 
                                  state=state, 
                                  level=node.level+1, 
                                  action=action)
                     q.add(child)
-
